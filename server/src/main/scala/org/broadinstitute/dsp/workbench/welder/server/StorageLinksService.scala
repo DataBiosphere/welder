47586411195304d2d06313eff5ca93d9fd5a3651
Matt Bemis <mbemis@broadinstitute.org>
2019-05-19 20:31:09 -0400

    get storage links and create storage link

--- server/src/main/scala/org/broadinstitute/dsp/workbench/welder/server/StorageLinksService.scala
+++ server/src/main/scala/org/broadinstitute/dsp/workbench/welder/server/StorageLinksService.scala
@@ -1,26 +1,85 @@
 package org.broadinstitute.dsp.workbench.welder.server
 
 import cats.effect.IO
+import io.circe.{Decoder, Encoder}
 import org.http4s.HttpRoutes
 import org.http4s.dsl.Http4sDsl
+import io.circe.parser._
+import io.circe.syntax._
+import org.http4s.circe.CirceEntityEncoder._
+import org.http4s.circe.CirceEntityDecoder._
+
+import scala.util.Try
 
 object StorageLinksService extends Http4sDsl[IO] {
+
   val service: HttpRoutes[IO] = HttpRoutes.of[IO] {
     case GET -> Root =>
       Ok(getStorageLinks())
-    case PUT -> Root =>
-      Ok(updateStorageLinks)
-    case POST -> Root =>
-      Ok(createStorageLinks())
+    case req @ DELETE -> Root =>
+      for {
+        storageLink <- req.as[StorageLink]
+        resp <- Ok(deleteStorageLink(storageLink))
+      } yield resp
+    case req @ POST -> Root =>
+      for {
+        storageLink <- req.as[StorageLink]
+        resp <- Ok(createStorageLink(storageLink))
+      } yield resp
   }
 
-  def createStorageLinks(): IO[Unit] = ???
+  def createStorageLink(storageLink: StorageLink): IO[Unit] = {
+    getStorageLinks().map { currentStorageLinks =>
+      val updatedStorageLinks = currentStorageLinks + storageLink
 
-  def updateStorageLinks(): IO[Unit] = ???
+      reflect.io.File("storagelinks.json").writeAll(updatedStorageLinks.asJson.toString)
+    }.map(_ => ())
+  }
 
-  def getStorageLinks(): IO[Unit] = ???
+  def deleteStorageLink(storageLink: StorageLink): IO[Unit] = {
+    getStorageLinks().map { currentStorageLinks =>
+      val updatedStorageLinks = currentStorageLinks - storageLink
+      reflect.io.File("storagelinks.json").writeAll(updatedStorageLinks.asJson.toString)
+    }.map(_ => ())
+  }
+
+  def getStorageLinks(): IO[Set[StorageLink]] = {
+    //TODO: handle file not existing
+    decode[Set[StorageLink]](loadStorageLinksFile) match {
+      case Left(_) => IO.pure(Set.empty) //TODO: actually handle error here
+      case Right(foo) => IO.pure(foo)
+    }
+  }
+
+  private def loadStorageLinksFile(): String = {
+    Try(scala.io.Source.fromFile("storagelinks.json").mkString).recover {
+      case _ => ""
+    }.get
+  }
+
 }
 
 final case class LocalDirectory(asString: String) extends AnyVal
 final case class GsDirectory(asString: String) extends AnyVal
-final case class StorageLinks(localBaseDirectory: LocalDirectory, cloudStorageDirectory: GsDirectory, pattern: String, recursive: Boolean)
+final case class StorageLink(localBaseDirectory: LocalDirectory, cloudStorageDirectory: GsDirectory, pattern: String, recursive: Boolean)
+
+object StorageLink {
+
+  implicit val localDirectoryEncoder: Encoder[LocalDirectory] = Encoder.encodeString.contramap(_.asString)
+  implicit val gsDirectoryEncoder: Encoder[GsDirectory] = Encoder.encodeString.contramap(_.asString)
+
+  implicit val localDirectoryDecoder: Decoder[LocalDirectory] = Decoder.decodeString.map(LocalDirectory)
+  implicit val gsDirectoryDecoder: Decoder[GsDirectory] = Decoder.decodeString.map(GsDirectory)
+
+  implicit val storageLinkEncoder: Encoder[StorageLink] = Encoder.forProduct4(
+    "localBaseDirectory",
+    "cloudStorageDirectory",
+    "pattern",
+    "recursive")(x => StorageLink.unapply(x).get)
+
+  implicit val storageLinkDecoder: Decoder[StorageLink] = Decoder.forProduct4(
+    "localBaseDirectory",
+    "cloudStorageDirectory",
+    "pattern",
+    "recursive")(StorageLink.apply)
+}
