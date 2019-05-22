defb9bab60e8933a0a973cb1c69b6947646ce717
Matt Bemis <mbemis@broadinstitute.org>
2019-05-22 18:04:07 -0400

    update ConfigSpec

--- server/src/test/scala/org/broadinstitute/dsp/workbench/welder/server/ConfigSpec.scala
+++ server/src/test/scala/org/broadinstitute/dsp/workbench/welder/server/ConfigSpec.scala
@@ -6,7 +6,7 @@ import org.scalatest.{FlatSpec, Matchers}
 class ConfigSpec extends FlatSpec with Matchers {
   "Config" should "read configuration correctly" in {
     val config = Config.appConfig
-    val expectedConfig = AppConfig("fakePath")
+    val expectedConfig = AppConfig("fakePath", "storageLinks.json")
     config shouldBe Right(expectedConfig)
   }
 }
