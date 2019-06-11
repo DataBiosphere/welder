3cbd4003bebc50fa785f8311f85889a00db6a8c5
CH Albach <calbach@users.noreply.github.com>
2019-06-11 15:51:43 -0400

    Drop unused /etc/welder.conf

--- project/Settings.scala
+++ project/Settings.scala
@@ -104,7 +104,7 @@ object Settings {
   lazy val serverDockerSettings = commonDockerSettings ++ List(
     mainClass in Compile := Some("org.broadinstitute.dsp.workbench.welder.server.Main"),
     packageName in Docker := "broad-dsp-gcr-public/welder-server",
-    dockerEntrypoint := List("/opt/docker/bin/server", "-Dconfig.file=/etc/welder.conf"), //TODO: put /etc/welder.conf on the image
+    dockerEntrypoint := List("/opt/docker/bin/server"),
     dockerAlias := DockerAlias(
       Some("us.gcr.io"),
       None,
