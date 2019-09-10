1031624035e43d6773c9b56503e4df8cad55a13b
Qi Wang <qi@broadinstitute.org>
2019-09-10 16:27:42 -0400

    persist WORKSPACE_BUCKET

--- server/src/test/scala/org/broadinstitute/dsp/workbench/welder/server/ConfigSpec.scala
+++ server/src/test/scala/org/broadinstitute/dsp/workbench/welder/server/ConfigSpec.scala
@@ -11,7 +11,7 @@ class ConfigSpec extends FlatSpec with Matchers {
   "Config" should "read configuration correctly" in {
     val config = Config.appConfig
     val objectServiceConfig = ObjectServiceConfig(Paths.get("/work"), WorkbenchEmail("fake@gmail.com"), 3 minutes)
-    val expectedConfig = AppConfig(8080, 7 minutes, 10 minutes, 15 minutes, Paths.get("/work/.welder/storage_links.json"), Paths.get("/work/.welder/gcs_metadata.json"), objectServiceConfig)
+    val expectedConfig = AppConfig(8080, 7 minutes, 10 minutes, 15 minutes, Paths.get("/work/.welder/storage_links.json"), Paths.get("/work/.welder/gcs_metadata.json"), Paths.get("/work/WORKSPACE_BUCKET"), objectServiceConfig)
     config shouldBe Right(expectedConfig)
   }
 }
