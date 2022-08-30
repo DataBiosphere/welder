cd4522f7414243698fed17af19ca06d573dc8539
Qi Wang <qi@broadinstitute.org>
2022-08-30 09:20:17 -0400

    update wb-libs

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,11 +2,11 @@ import sbt._
 
 object Dependencies {
   val circeVersion = "0.14.2"
-  val http4sVersion = "1.0.0-M34"
+  val http4sVersion = "1.0.0-M35"
   val grpcCoreVersion = "1.34.0"
   val scalaTestVersion = "3.2.13"
 
-  val workbenchLibsHash = "b06385e8-SNAP"
+  val workbenchLibsHash = "0096bac"
   val workbenchGoogle2V = s"0.24-$workbenchLibsHash"
   val workbenchAzureV = s"0.1-$workbenchLibsHash"
 
