ebc80a6a740ea403103d5ddc2d25d69070653ec3
broadbot <broadbot@broadinstitute.org>
2022-12-02 12:02:28 +0000

    Revert commit(s) 66f646c

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
   val circeVersion = "0.14.2"
-  val http4sVersion = "1.0.0-M36"
+  val http4sVersion = "1.0.0-M35"
   val grpcCoreVersion = "1.49.2"
   val scalaTestVersion = "3.2.14"
 
