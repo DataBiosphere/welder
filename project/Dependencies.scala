e8daacab8eb2655f1a00652a461af0108c44485f
broadbot <broadbot@broadinstitute.org>
2023-08-31 12:03:52 +0000

    Update circe-core, circe-generic, ... to 0.14.6

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 object Dependencies {
-  val circeVersion = "0.14.3"
+  val circeVersion = "0.14.6"
   val http4sVersion = "1.0.0-M35"
   val grpcCoreVersion = "1.51.0"
   val scalaTestVersion = "3.2.14"
