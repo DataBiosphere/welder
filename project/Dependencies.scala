da6a7181596772bb1b6864c9f453e67518c9dd61
broadbot <broadbot@broadinstitute.org>
2023-10-06 12:04:24 +0000

    Update http4s-blaze-client, ... to 1.0.0-M39

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
   val circeVersion = "0.14.3"
-  val http4sVersion = "1.0.0-M35"
+  val http4sVersion = "1.0.0-M39"
   val grpcCoreVersion = "1.51.0"
   val scalaTestVersion = "3.2.14"
 
